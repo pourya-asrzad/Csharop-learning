04d53f3988dc03e2651a3be7d5b507bd255b9fd1
Pouria Asrzad <poriaasrzad@gmail.com>
2023-10-01 19:48:03 +0330

    structur added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-Console.WriteLine("fdgfdg");
+namespace HelloWold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
