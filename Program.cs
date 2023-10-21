563ce0f068abfa7c5cf31c0332ac3bf3142a7c67
Pouria Asrzad <poriaasrzad@gmail.com>
2023-10-21 15:39:26 +0330

    add new string and forech

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HelloWold
 {
-
     class Car
     {
         public string Name;
@@ -15,22 +15,26 @@ namespace HelloWold
         public int Wheels;
         public string Color;
         public int Speed;
+        public string[] additionalThings;
         public bool IsStandard;
-        public void printAtr()    // method
+        public void printAtr()
         {
+            // will print all properties in the console 
             Console.WriteLine("\n Name:{0} , Brand :{1} , color : {2} , price : {3} , Wheels : {4} , Speed : {5} , IsStandard : {6}", Name, Brand, Color, Price, Wheels, Speed, IsStandard);
+            foreach (var s in additionalThings)
+            {
+                Console.WriteLine(s);
+            }
         }
-        public void speedUp()    // method
+        public void speedUp()
         {
+            // increase speed one bye one after using method
             Speed++;
-
         }
-
-        public void breakUp()    // method
+        public void breakUp()
         {
+            // decrease the speed 10 km/h after using that
             Speed -= 10;
-
-
         }
     }
     class Program
@@ -38,11 +42,15 @@ namespace HelloWold
         static void Main(string[] args)
         {
             Car myObj = new Car();
+
             myObj.Brand = "saipa";
             myObj.Name = "pride";
             myObj.Price = 800000;
             myObj.Wheels = 4;
             myObj.Color = "blue";
+            myObj.additionalThings = new string[5];
+            myObj.additionalThings[0] = "df";
+            myObj.additionalThings[1] = "seconf";
             myObj.Speed = 180;
             myObj.IsStandard = false;
             myObj.printAtr();
@@ -52,7 +60,7 @@ namespace HelloWold
             myObj.speedUp();
             myObj.breakUp();
             Console.WriteLine(myObj.Speed);
-            Console.WriteLine(myObj.Color);
+            // Console.WriteLine(myObj.Color);
 
         }
     }
