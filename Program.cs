aaddd705556658de39d871357365fbc2754d9de2
pourya-asrzad <poriaasrzad@gmail.com>
2023-10-28 14:31:37 +0330

    linux : commit

--- Program.cs
+++ Program.cs
@@ -1,65 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
+using System;
 namespace HelloWold
 {
-    class Car
+    class Rectangle
     {
-        public string Name;
-        public string Brand;
-        public float Price;
-        public int Wheels;
-        public string Color;
-        public int Speed;
-        public string[] additionalThings;
-        public bool IsStandard;
-        public void printAtr()
+        
+        private int width ;
+        private int height;
+
+
+        public int Width{
+            get{return width;}
+            set{if(value>0) width = value;else throw new ArgumentOutOfRangeException("what are you doing bro");}
+        }
+        public int Height{
+            get{return height;}
+            set{if(value>0) height = value;else  throw new ArgumentOutOfRangeException("what are you doing bro");}
+        }
+
+        public int Area() {return Height * Width;}
+        public int Environment() {return 2*(Height + Width);}
+        public void printInfo()
         {
             // will print all properties in the console 
-            Console.WriteLine("\n Name:{0} , Brand :{1} , color : {2} , price : {3} , Wheels : {4} , Speed : {5} , IsStandard : {6}", Name, Brand, Color, Price, Wheels, Speed, IsStandard);
-            foreach (var s in additionalThings)
-            {
-                Console.WriteLine(s);
-            }
+            
+            Console.WriteLine("\n area:{0} \n Environment{1}", Height*Width, 2*(Width+Height));
+            
         }
-        public void speedUp()
-        {
-            // increase speed one bye one after using method
-            Speed++;
-        }
-        public void breakUp()
-        {
-            // decrease the speed 10 km/h after using that
-            Speed -= 10;
-        }
+        // public void speedUp()
+        // {
+            
+        // }
+       
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Car myObj = new Car();
-
-            myObj.Brand = "saipa";
-            myObj.Name = "pride";
-            myObj.Price = 800000;
-            myObj.Wheels = 4;
-            myObj.Color = "blue";
-            myObj.additionalThings = new string[5];
-            myObj.additionalThings[0] = "df";
-            myObj.additionalThings[1] = "seconf";
-            myObj.Speed = 180;
-            myObj.IsStandard = false;
-            myObj.printAtr();
-            myObj.speedUp();
-            myObj.speedUp();
-            myObj.speedUp();
-            myObj.speedUp();
-            myObj.breakUp();
-            Console.WriteLine(myObj.Speed);
+         Rectangle myObj = new Rectangle();
+         myObj.Height = 10;
+         myObj.Width =20;
+         myObj.printInfo();
+            
             // Console.WriteLine(myObj.Color);
 
         }
